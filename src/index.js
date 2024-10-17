8ebee8f9cd4b71bf6d9a2e9162a29159952574b9
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2024-10-17 18:15:25 +0300

    solved brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let bracketMap = {};
+
+  bracketsConfig.forEach(([open, close]) => {
+    bracketMap[open] = close;
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    let bracket = str[i];
+    if (bracket === bracketMap[bracket]) {
+      if (stack[stack.length - 1] === bracket) {
+        stack.pop();
+      } else {
+        stack.push(bracket);
+      }
+      continue;
+    }
+    // для работы с обычными скобками {}/[]/()
+    if (bracket in bracketMap) {
+      stack.push(bracket);
+    } else {
+      if (bracket !== bracketMap[stack.pop()]) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
